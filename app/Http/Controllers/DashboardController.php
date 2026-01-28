b48313e6e780d68335a3e5a6f90ca77aa40a95cd
yoguud <yogadwifadillah@gmail.com>
2026-01-28 14:20:57 +0700

    new web php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Film;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $films = Film::all();
+        $moviesByGenre = Film::all()->groupBy('genre');
+        return view('dashboard',[
+            'films' => $films,
+            'moviesByGenre' => $moviesByGenre,
+        ]);
+    }
 }
