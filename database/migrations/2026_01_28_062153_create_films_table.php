782ab7fd6ef0b6875e4310d88490aafc21752ca9
yoguud <yogadwifadillah@gmail.com>
2026-01-28 13:29:56 +0700

    create film model and database migration for film

---
+++ database/migrations/2026_01_28_062153_create_films_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('poster');
+            $table->string('genre');
+            $table->date('release_date');
+            $table->integer('duration'); // duration in minutes
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
